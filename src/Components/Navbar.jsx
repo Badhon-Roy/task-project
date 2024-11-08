cfc4710fb3b721a561bb7ad7f55864a85e9d0452
Badhon-Roy <roybadhon286@gmail.com>
2024-11-08 22:01:07 +0600

    navbar created successfully

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,24 +1,30 @@
+import { FaMobileAlt, FaRegCreditCard } from "react-icons/fa";
+import { FaBuildingColumns, FaHouse, FaUserLarge } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="flex justify-around gap-2 mt-[10px] pb-1 shadow-top-only pt-3 rounded-b-[20px]">
             <NavLink
-                to={'/'} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""}>
-                Home
+                to={'/'} className={({ isActive, isPending }) => isActive ? "primaryColor text-[14px]" : isPending ? "pending" : ""}>
+                <span className="flex flex-col items-center justify-center"><FaHouse className="text-[18px]" /> Home</span>
             </NavLink>
             <NavLink
-                to={'/'} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""}>
-                Home
+                to={'/'} className={({ isActive, isPending }) => isActive ? "primaryColor text-[14px]" : isPending ? "pending" : ""}>
+                <span className="flex flex-col items-center justify-center"><FaMobileAlt className="text-[18px]" /> MFS</span>
             </NavLink>
             <NavLink
-                to={'/'} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""}>
-                Home
+                to={'/'} className={({ isActive, isPending }) => isActive ? "primaryColor text-[14px]" : isPending ? "pending" : ""}>
+                <span className="flex flex-col items-center justify-center">   <FaBuildingColumns className="text-[18px]"/> Bank</span>
             </NavLink>
             <NavLink
-                to={'/'} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""}>
-                Home
+                to={'/'} className={({ isActive, isPending }) => isActive ? "primaryColor text-[14px]" : isPending ? "pending" : ""}>
+                <span className="flex flex-col items-center justify-center"> <FaRegCreditCard className="text-[18px]"/> Card</span>
+            </NavLink>
+            <NavLink
+                to={'/'} className={({ isActive, isPending }) => isActive ? "primaryColor text-[14px]" : isPending ? "pending" : ""}>
+                <span className="flex flex-col items-center justify-center"><FaUserLarge className="text-[18px]" /> Profile</span>
             </NavLink>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
   theme: {
     extend: {
       boxShadow : {
-        'custom' : '2px 4px 4px rgba(33, 33, 33)'
+        'custom' : '2px 4px 4px rgba(33, 33, 33)',
+        'top-only': '0 -4px 6px rgba(0, 0, 0, 0.1)'
       }
     },
   },
